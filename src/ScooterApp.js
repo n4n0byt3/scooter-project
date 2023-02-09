b070b0ba93c27688b91ce5d59987d324a1cc989d
n4n0byt3 <shivbo1@hotmail.com>
2023-02-09 16:32:49 +0000

    completed registerUser function method

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -6,26 +6,29 @@ class ScooterApp {
     this.stations = stations
     this.registeredUsers = registeredUsers
   }
-  registerUser = function(username, password, age) {
-    for (var i = 0; i < registeredUsers.length; i++) {
-      if (this.user[i].username === username) {
-        return console.log('Already registered');
+
+  registerUser(username, password, age) {
+    for (let i = 0; i < Object.keys(this.registeredUsers).length; i++) {
+      if (Object.keys(this.registeredUsers)[i] == username) {
+        return console.log("already registered");
       }
     }
-  
     if (age < 18) {
-      return console.log('Too young to register');
+      return console.log("too young to register");
     }
+    this.registeredUsers[username] = "";
+    console.log("user has been added")
+  }
   
-    this.users.push({
-      username: username,
-      password: password,
-      age: age
-    });
-  
-    console.log('User ' + username + ' has been registered');
-  };
+ 
 
 }
-
+// user1 = {
+//   name: "pass",
+//   name2: "word"
+// }
+//  const test = new ScooterApp (user1)
+//  console.log(test.registerUser("name2"))
+//  console.log(Object.keys(test.registeredUsers))
+//  console.log((Object.keys(user1))[0])
 module.exports = ScooterApp
