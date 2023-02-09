b27ad31979afc84b0ba39ab4f20704d0d49d019f
n4n0byt3 <shivbo1@hotmail.com>
2023-02-09 17:17:46 +0000

    completed logging in and out functions scooterApp

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -2,8 +2,9 @@ const User = require('./User')
 const Scooter = require('./Scooter')
 
 class ScooterApp {
-  constructor(stations, registeredUsers) {
-    this.stations = stations
+  //constructor(stations, retisteredUsers) <--- correct params 
+  constructor( registeredUsers) {
+    //this.stations = stations
     this.registeredUsers = registeredUsers
   }
 
@@ -19,10 +20,36 @@ class ScooterApp {
     this.registeredUsers[username] = "";
     console.log("user has been added")
   }
+  loginUser(username, password) {
+    let bool = false;
+    for (let i = 0; i < Object.keys(this.registeredUsers).length; i++) {
+      if (username == (Object.keys(this.registeredUsers)[i]) && password == (Object.values(this.registeredUsers)[i])) {
+      console.log("user has been logged in")
+      bool = true
+      break;
+    } 
+    }
+    if (!bool) {
+      console.log("incorrect username or password")
+    }
+  }
   
- 
+  logOutUser (username) {
+    let bool = false
+    for (let i = 0; i < Object.keys(this.registeredUsers).length; i++) {
+      if (username == (Object.keys(this.registeredUsers)[i])) {
+        console.log("user has been logged out");
+        bool = true
+        break;
+      } 
+      
+    }
+    if (!bool) {
+      console.log("no such user was logged in")
+    }
+  }
 
-}
+}//last curly bracket for class
 // user1 = {
 //   name: "pass",
 //   name2: "word"
@@ -31,4 +58,6 @@ class ScooterApp {
 //  console.log(test.registerUser("name2"))
 //  console.log(Object.keys(test.registeredUsers))
 //  console.log((Object.keys(user1))[0])
+// const test = new ScooterApp (user1)
+// console.log(test.logOutUser("name", "pass"))
 module.exports = ScooterApp
