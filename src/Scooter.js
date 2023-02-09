6cf18405f0093b109cbd46b9c29015d703049bf2
n4n0byt3 <shivbo1@hotmail.com>
2023-02-09 16:01:59 +0000

    managed to complete scooter and user classes

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,6 +1,55 @@
 class Scooter{
-  // scooter code here
+  constructor (station, user, seial, nextSerial, charge, isBroken) {
+    this.station = station
+    this.user = user
+    this.serial = serial
+    this.nextSerial = nextSerial
+    this.charge = charge
+    this.isBroken = isBroken
+  }
+  rent () {
+    if (this.charge > 20 && this.isBroken === false) {
+      this.station === null;
+    } else {
+      this.user === null;
+      if (this.charge < 20) {
+        throw new Error ("scooter needs to charge")
+      } else if (this.isBroken === true) {
+        throw new Error ("scooter needs to be repaired")
+      }
+    }
+  }
+  dock(station) {
+    this.station === station
+    this.user === null;
+  }
+
+  recharge() {
+    console.log('Current charge: ' + this.charge);
+  let interval = setInterval(function() {
+    this.charge++;
+    if (this.charge % 20 === 0) {
+      console.log('Charge is now at ' + this.charge);
+    }
+    if (this.charge === 100) {
+      clearInterval(interval);
+      console.log("Scooter fully charged");
+    }
+  }.bind(this), 1000);
 }
 
+  requestRepair () {
+    console.log(this.isBroken)
+    let interval = setInterval(function() {
+      this.broken = false;
+      console.log("repair completed")
+      clearInterval(interval);
+    }.bind(this), 5000);
+  }
+  
 
+}
+
+// const test = new Scooter (true)
+// console.log(test.requestRepair())
 module.exports = Scooter
--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -2,7 +2,30 @@ const User = require('./User')
 const Scooter = require('./Scooter')
 
 class ScooterApp {
-  // ScooterApp code here
+  constructor(stations, registeredUsers) {
+    this.stations = stations
+    this.registeredUsers = registeredUsers
+  }
+  registerUser = function(username, password, age) {
+    for (var i = 0; i < registeredUsers.length; i++) {
+      if (this.user[i].username === username) {
+        return console.log('Already registered');
+      }
+    }
+  
+    if (age < 18) {
+      return console.log('Too young to register');
+    }
+  
+    this.users.push({
+      username: username,
+      password: password,
+      age: age
+    });
+  
+    console.log('User ' + username + ' has been registered');
+  };
+
 }
 
 module.exports = ScooterApp
